30cd4a9537f712e91949aa5cf3b5843fa18a8557
ulfsv <ulf.e.sv@gmail.com>
2020-08-27 12:16:58 +0200

    Add project files.

---
+++ FlowControl/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace FlowControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool showMenu = true;
+            while (showMenu)
+            {
+                showMenu = MainMenu();
+            }
+        }
+        private static bool MainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Huvudmeny. Skriv in siffror för att testa olika funktioner:");
+            Console.WriteLine("0) Exit");
+            Console.Write("\r\nTesta: ");
+
+            switch (Console.ReadLine())
+            {
+                case "0":
+                    //Exit application
+                    return false;
+                default:
+                    _ = CaptureInput();
+                    return true;
+            }
+        }
+        private static string CaptureInput()
+        {
+            Console.Write("Felaktig inmatning! (Enter för huvudmeny.)");
+            return Console.ReadLine();
+        }
+        private static void DisplayResult(string message)
+        {
+            Console.WriteLine($"\r\nInput: {message}");
+            Console.Write("Felaktig inmatning!");
+            Console.Write("Tryck Enter för Huvudmeny");
+            Console.ReadLine();
+        }
+    }
+}
